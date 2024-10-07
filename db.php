94bcf3318c74ed05cfff6a80ab6e924acf57f1a1
sumukhanidhibs <sumukhanidhi@gmail.com>
2024-10-07 11:46:50 +0530

    pdo updated

--- db.php
+++ db.php
@@ -1,1 +1,12 @@
 <?php
+
+require "functions.php";
+$username="sumukha";
+$pass="1144";
+
+$dsn = "mysql:host=localhost;port=3306;dbname=todo;charset=utf8mb4";
+$query = "select * from posts;";
+
+$pdo = new PDO($dsn,$username,$pass);
+
+$statement = $pdo->prepare($query);
